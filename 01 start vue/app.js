b933954bb364057123840794fb3184ac34d7d80a
静默 <cau1403090523@gmail.com>
2021-12-02 11:31:51 +0800

    multiple vue instance

--- 01 start vue/app.js
+++ 01 start vue/app.js
@@ -3,10 +3,18 @@ const vm = Vue.createApp({
   data() {
     return {
       msg: "Hello Vue",
+      firstName: "Q10",
+      lastName: "Viking",
     };
+  },
+
+  methods: {
+    fullName() {
+      return `${this.firstName} ${this.lastName.toUpperCase()}`;
+    },
   },
 }).mount("#app");
 
-setTimeout(() => {
-    vm.msg = "Learning Vue FrameWork"
-}, 2000);
+// setTimeout(() => {
+//   vm.msg = "Learning Vue FrameWork";
+// }, 2000);
---
+++ 02 multiple vue instance/app.js
@@ -0,0 +1,4 @@
+// Vue Model
+const vm1 = Vue.createApp({}).mount("#app-test1");
+const vm2 = Vue.createApp({}).mount("#app-test2");
+
