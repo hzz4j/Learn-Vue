0542d869a686d0d86eca0658fbed6893fa394ae7
静默 <cau1403090523@gmail.com>
2021-12-02 14:28:01 +0800

    event prevetn

--- 05 listening on events/app.js
+++ 05 listening on events/app.js
@@ -3,11 +3,18 @@ const vm = Vue.createApp({
   data() {
     return {
       count: 0,
+      username: "",
     };
   },
   methods: {
     incrementCount(event) {
       this.count++;
+    },
+    updateUsername(msg, event) {
+      // 使用修饰符来作用
+      // event.preventDefault();
+      console.log(msg);
+      this.username = event.target.value;
     },
   },
 }).mount("#app");
