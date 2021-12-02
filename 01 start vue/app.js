46687b4eec87a059b1e71ee51489fbbc2f4c59f9
静默 <cau1403090523@gmail.com>
2021-12-02 10:57:20 +0800

    Update app.js

--- 01 start vue/app.js
+++ 01 start vue/app.js
@@ -1,7 +1,12 @@
-Vue.createApp({
-    data(){
-        return {
-            msg: "Hello Vue"
-        }
-    }
-}).mount('#app')
+// Vue Model
+const vm = Vue.createApp({
+  data() {
+    return {
+      msg: "Hello Vue",
+    };
+  },
+}).mount("#app");
+
+setTimeout(() => {
+    vm.msg = "Learning Vue FrameWork"
+}, 2000);
