fe6dc1e145318836058087dde31307e7162854ff
静默 <cau1403090523@gmail.com>
2021-12-02 13:46:32 +0800

    output raw html

--- 04 Two ways bind data/app.js
+++ 04 Two ways bind data/app.js
@@ -1,10 +1,11 @@
 // Vue Model
 const vm = Vue.createApp({
-    data(){
-        return {
-            userName: "静默",
-            description: "Learning Vue"
-        }
-    }
+  data() {
+    return {
+      userName: "静默",
+      description: "Learning Vue",
+      blogLink: "https://q10viking.github.io",
+      raw_url: '<a href="https://q10viking.github.io" target="_blank">静默 Blog </a>',
+    };
+  },
 }).mount("#app");
-
