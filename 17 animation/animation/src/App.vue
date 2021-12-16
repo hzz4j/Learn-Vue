2eddde8ef7f61e54cc7bb4500a39367661a4582c
静默 <cau1403090523@gmail.com>
2021-12-16 10:01:21 +0800

    init project

---
+++ 17 animation/animation/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="flag = !flag">Toggle Button</button>
+  <h1 v-if="flag">Hello Animation</h1>
+</template>
+
+<script>
+export default {
+  name:'App',
+  data(){
+    return {
+      flag: false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
