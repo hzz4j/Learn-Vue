a2cd204bea730dc3f8f2b3eb7e8c4a3bf5fbfff5
静默 <cau1403090523@gmail.com>
2021-12-16 10:21:00 +0800

    Update App.vue

--- 17 animation/animation/src/App.vue
+++ 17 animation/animation/src/App.vue
@@ -1,19 +1,32 @@
 <template>
   <button @click="flag = !flag">Toggle Button</button>
-  <h1 v-if="flag">Hello Animation</h1>
+  <transition name="fade">
+    <h1 v-if="flag">Hello Animation</h1>
+  </transition>
 </template>
 
 <script>
 export default {
-  name:'App',
-  data(){
+  name: "App",
+  data() {
     return {
-      flag: false
-    }
-  }
-}
+      flag: false,
+    };
+  },
+};
 </script>
 
 <style>
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 2.25s linear;
+}
+
+.fade-leave-to{
+  opacity: 0;
+}
 
 </style>
